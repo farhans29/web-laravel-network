41bc37255d3d34065fde04e94ade47266f69cd24
M Farhan Syihab <m.farhanshihab11@gmail.com>
2025-02-11 21:21:03 +0700

    fix CORS

--- resources/views/pages/mikrotik/interfaces-list.blade.php
+++ resources/views/pages/mikrotik/interfaces-list.blade.php
@@ -50,8 +50,8 @@
             let urlParts = window.location.pathname.split("/");
             let routerId = urlParts[urlParts.length - 1]; // Get last segment
             $.ajax({
-                // url: "http://network.integrated-os.cloud/mikrotik/interfaces/getDataJson/?idr=1",
-                url: "http://localhost:8000/mikrotik/interfaces/getDataJson/?idr=1",
+                url: "http://network.integrated-os.cloud/mikrotik/interfaces/getDataJson/?idr=1",
+                // url: "http://localhost:8000/mikrotik/interfaces/getDataJson/?idr=1",
                 type: "GET",
                 data: { idr: routerId }, // Dynamically set router ID
                 success: function (response) {
